3072e52844c43538bd02498df28873b6bbb91b64
Sufail <muhammedsufailam@gmail.com>
2025-07-18 11:43:18 +0530

    Time: 0 ms (100%), Space: 43.2 MB (27.99%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+int a=0;
+int n = nums.length;
+    for(int i=0;i<nums.length;i++){
+        if(nums[i] >= target){
+         return i;  
+        }
+
+        }
+return n;
+
+    }
+ }
